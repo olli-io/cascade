caafd8fb56c1bd593dc4b5becea3ef267b638165
Olli Moisio <olli.moisio@protonmail.com>
2025-12-25 19:06:18 +0200

    Finalize customizations

--- up.sh
+++ up.sh
@@ -1,5 +1,10 @@
-echo "Applying profile to Firefox..."
-cp -r ~/git/cascade/chrome ~/Library/Application\ Support/Firefox/Profiles/*.default-release/
+if [ "$1" = "--uninstall" ]; then
+    echo "Removing profile from Firefox..."
+    rm -rf ~/Library/Application\ Support/Firefox/Profiles/*.default-release/chrome
+else
+    echo "Applying profile to Firefox..."
+    cp -r ~/git/cascade/chrome ~/Library/Application\ Support/Firefox/Profiles/*.default-release/
+fi
 echo "Restarting Firefox..."
 osascript -e 'tell application "Firefox" to quit' 2>/dev/null || killall Firefox 2>/dev/null || true
 sleep 1
